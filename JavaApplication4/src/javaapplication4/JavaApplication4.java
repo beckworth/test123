f7a858775d78c8137037ecd99c11416a5a0da6d7
mikeb <mikeb@beckworth-nb7.bex-components.de>
2012-09-28 17:00:17 +0200

    gggggg

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -14,6 +14,6 @@ public class JavaApplication4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic herecccccmmmmmvvvvv
+        // TODO code application logic herecccccmmmmmvvvvvttttt
     }
 }
