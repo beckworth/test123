7d496e564351075975b338f8fd8a5026989ebdd4
mikeb <mikeb@beckworth-nb7.bex-components.de>
2012-09-28 13:54:25 +0200

    ddddd

--- test/SampleGit/src/test/HelloWorld.java
+++ test/SampleGit/src/test/HelloWorld.java
@@ -1,6 +1,15 @@
 package test;
 
-public class HelloWorld {
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class HelloWorld extends JFrame{
+
+	public HelloWorld() throws HeadlessException {
+		super();
+		setVisible(true);
+	}
 
 	/**
 	 * @param args
@@ -8,6 +17,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		new HelloWorld();
+		
+		
 	}
 
 }
